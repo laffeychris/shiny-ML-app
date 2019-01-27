e87a723f2e0fcce3a1588651d6a9b3aec29efd75
LaffeyChris <christopher.laffey1995@gmail.com>
2019-01-27 11:46:41 +0000

    Found latest version of project on PC. updated to match

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 shinyServer(function(input, output){
+  #load all packages 
   library(ROCR)
   library(caret)
   library(glmnet)
@@ -31,10 +32,15 @@ shinyServer(function(input, output){
     df
   })
   
+  #Assign K-fold variable
+  kfold<-reactive({
+    as.integer(input$k_fold)
+  })
+  #Create folds 
   cvIndex <- reactive({
     set.seed(222)
     df<-datasetInput1()
-    createFolds(factor(df[,c(input$dv)]),  3, returnTrain = T)
+    createFolds(factor(df[,c(input$dv)]), k = kfold() , returnTrain = T)
   })
   
   #Create empty vectors to store indices
@@ -42,67 +48,70 @@ shinyServer(function(input, output){
   #check trainset output
   
   
-  #loop for k=3 folds
+  #loop for no. of folds
   #create test, train, and answers
   trainset<- reactive({
     df<- datasetInput()
     cv<-cvIndex()
     trainset<-NULL
-    for(i in 1:3){
+    for(i in 1:kfold()){
       trainset[[i]]<-df[c(cv[[i]]), ]
     }
     return(trainset)})
   
-  #test
+  #test 
   testset<- reactive({
     df<- datasetInput()
     cv<-cvIndex()
     testset<-NULL
-    for(i in 1:3){
+    for(i in 1:kfold()){
       testset[[i]]<-df[-c(cv[[i]]), ]
     }
     return(testset)
   })
   
+  #response variable for test sets
   answers<- reactive({
     test_df<-testset()
     answers<-NULL
-    for(i in 1:3){
+    for(i in 1:kfold()){
       answers[[i]]<-test_df[[i]][,c(input$dv)]
     }
     return(answers)
   })
   
   
-  #Matrices for our obrf model 
+  #Matrices for our obrf model
+  #some models cannot accept formulae as arguments
   trainset_x<- reactive({
     trainset_x<-NULL
     train_df<- trainset()
     drops<- c(input$dv)
-    for (i in 1:3){
+    for (i in 1:kfold()){
       trainset_x[[i]]<-as.matrix(train_df[[i]][ , !(names(datasetInput()) %in% drops)])
     }
     return(trainset_x)
   })
   
+  #response variable of the training set
   trainset_y<- reactive({
     trainset_y<-NULL
     train_df<- trainset()
-    for (i in 1:3){
+    for (i in 1:kfold()){
       trainset_y[[i]]<-as.factor(train_df[[i]][ ,c(input$dv)]==c(levelset()[1]))
     }
     return(trainset_y)
   })
-  
+  #test set predictor variables
   testset_x<- reactive({
     testset_x<-NULL
     drops<- c(input$dv)
-    for (i in 1:3){
+    for (i in 1:kfold()){
       testset_x[[i]]<-as.matrix(testset()[[i]][ , !(names(datasetInput()) %in% drops)])
     }
     testset_x
   })
-  
+  #The class lables of the dependant variable
   levelset<-reactive({
     levels(datasetInput()[,c(input$dv)])
   })
@@ -127,13 +136,20 @@ shinyServer(function(input, output){
     p
   })
   
+  
+  #Wilcoxon statistic
+  output$wilcoxon<- renderPrint({
+    A<- subset(datasetInput(), datasetInput()[,c(input$dv)] == c(levelset()[1]), select = c(input$sumvar))[,1]
+    B<- subset(datasetInput(), datasetInput()[,c(input$dv)] == c(levelset()[2]), select = c(input$sumvar))[,1]
+    wilcox.test(A,B, paired = TRUE)
+  })
+  
   #Regression formulae
   regFormula <- reactive({
     as.formula(paste(input$dv, '~', '.'))
   })
   
-  #make single reactive models for our VarImpPlots and summaries.
-  
+  #make single reactive models for our VarImpPlots
   rf_model<-reactive({
     randomForest(regFormula(), data = datasetInput(), ntree = input$rf_tree)
   })
@@ -142,11 +158,6 @@ shinyServer(function(input, output){
     drops<-c(input$dv)
     dataset_x<-as.matrix(datasetInput()[ , !(names(datasetInput()) %in% drops)])
     cv.glmnet(x = dataset_x, y = datasetInput()[,c(input$dv)], family='binomial', alpha = 1)
-  })
-  
-  
-  svm_model<-reactive({
-    svm(regFormula(), data = datasetInput(), probability = TRUE)
   })
   
   ada_model<-reactive({
@@ -164,37 +175,36 @@ shinyServer(function(input, output){
     mod
   })
   
-  output$type<-renderPrint({
-    imp<-obrf_model()$importance
-    str(imp)
-  })
   
   
   #Probabilities---------------------------------------
-  
+  #Begin creating models for each fold in k-folds.
+  #Here we store all the predictive probabilities of our models
   lasso_pred_prob<-reactive({
     lasso_pred_prob<-NULL
-    for (i in 1:3){
-      lasso_model<-cv.glmnet(trainset_x()[[i]], y = trainset_y()[[i]], family='binomial', alpha = 1, nfolds = 4)
+    for (i in 1:kfold()){
+      lasso_model<-cv.glmnet(trainset_x()[[i]], y = trainset_y()[[i]], family='binomial', alpha = 1, nfolds = 5)
       lasso_pred_prob1<-predict(lasso_model, newx= testset_x()[[i]], type='response',s=c(input$t_value))
       lasso_pred_prob[[i]]<-lasso_pred_prob1
     }
     return(lasso_pred_prob)
   })
   
-  
+  #Store predictive probabilities of our Random Forest
   rf_pred_prob<-reactive({
     rf_pred_prob<-NULL
-    for (i in 1:3){
+    for (i in 1:kfold()){
       rf_model<-randomForest(regFormula(), data = trainset()[[i]], ntree = input$rf_tree)
       rf_pred_prob[[i]]<-predict(rf_model, newdata = testset()[[i]], type ='prob')[,c(levelset()[2])]
     }
     return(rf_pred_prob)
   })
   
+  
+  #Again for our SVM 
   svm_pred_prob<-reactive({
     svm_pred_prob<-NULL
-    for (i in 1:3){
+    for (i in 1:kfold()){
       svm_model<-svm(regFormula(), data = trainset()[[i]], probability = TRUE, cost = input$svm_cost)
       svm_pred_prob1<- predict(svm_model,newdata = testset()[[i]] ,decision.values =TRUE,
                                probability = TRUE)
@@ -203,10 +213,10 @@ shinyServer(function(input, output){
     return(svm_pred_prob)
   })
   
-  
+  #Again for our AdaBoost model
   ada_pred_prob<-reactive({
     ada_pred_prob<-NULL
-    for (i in 1:3){
+    for (i in 1:kfold()){
       ada_model<-boosting(regFormula(),data=trainset()[[i]], mfinal = input$ada_tree)
       ada_pred_prob1<- predict(ada_model ,newdata = testset()[[i]],type='prob')$prob
       ada_pred_prob[[i]]<- ada_pred_prob1[,2]
@@ -214,9 +224,10 @@ shinyServer(function(input, output){
     return(ada_pred_prob)
   })
   
+  #Oblique random forest - logistic node split
   obrf_pred_prob<-reactive({
     obrf_pred_prob<-NULL
-    for (i in 1:3){
+    for (i in 1:kfold()){
       obrf_model<-obliqueRF(trainset_x()[[i]], trainset_y()[[i]], ntree = input$obrf_tree, training_method = 'log')
       obrf_pred_prob1<-predict(obrf_model,newdata = testset_x()[[i]], type='prob')
       colnames(obrf_pred_prob1)<-c(levelset()[1], levelset()[2])
@@ -225,18 +236,17 @@ shinyServer(function(input, output){
     return(obrf_pred_prob)
   })
   
-  output$type1<-renderPrint({
-    colnames(obrf_pred_prob()[[1]])
-  })
-  
+
   
   
   #Classes----------------------------------------------
   
+  #Round the probabilities of the models up to their respective lables.
+  #This will denote the class prediction (instead of having to re-run models)
   lasso_pred_class<-reactive({
     lasso_pred_class<-NULL
     lasso_pred_prob1<-lasso_pred_prob()
-    for (i in 1:3){
+    for (i in 1:kfold()){
       lasso_pred_class1<-ifelse(lasso_pred_prob1[[i]] > 0.5 ,levelset()[1],levelset()[2])
       lasso_pred_class[[i]]<-as.data.frame(lasso_pred_class1)[,1]
     }
@@ -248,7 +258,7 @@ shinyServer(function(input, output){
   rf_pred_class<-reactive({
     rf_pred_class<-NULL
     rf_pred_prob1<-rf_pred_prob()
-    for (i in 1:3){
+    for (i in 1:kfold()){
       rf_pred_class1<-ifelse(rf_pred_prob1[[i]] > 0.5 ,levelset()[2],levelset()[1])
       rf_pred_class[[i]]<-as.data.frame(rf_pred_class1)[,1]
     }
@@ -258,7 +268,7 @@ shinyServer(function(input, output){
   svm_pred_class<-reactive({
     svm_pred_class<-NULL
     svm_pred_prob1<-svm_pred_prob()
-    for (i in 1:3){
+    for (i in 1:kfold()){
       svm_pred_class1<-ifelse(svm_pred_prob1[[i]] > 0.5 ,levelset()[2],levelset()[1])
       svm_pred_class[[i]]<-as.data.frame(svm_pred_class1)[,1]
     }
@@ -269,7 +279,7 @@ shinyServer(function(input, output){
   ada_pred_class<-reactive({
     ada_pred_class<-NULL
     ada_pred_prob1<-ada_pred_prob()
-    for (i in 1:3){
+    for (i in 1:kfold()){
       ada_pred_class1<-ifelse(ada_pred_prob1[[i]] > 0.5 ,levelset()[2],levelset()[1])
       ada_pred_class[[i]]<-as.data.frame(ada_pred_class1)[,1]
     }
@@ -279,7 +289,7 @@ shinyServer(function(input, output){
   obrf_pred_class<-reactive({
     obrf_pred_class<-NULL
     obrf_pred_prob1<-obrf_pred_prob()
-    for (i in 1:3){
+    for (i in 1:kfold()){
       obrf_pred_class1<-ifelse(obrf_pred_prob1[[i]] > 0.5 ,levelset()[2],levelset()[1])
       obrf_pred_class[[i]]<-as.data.frame(obrf_pred_class1)[,1]
     }
@@ -289,6 +299,9 @@ shinyServer(function(input, output){
   
   #Accuracies------------------------------------
   
+  #Calculate accuracy for each model.
+  #Unlist the class predictions of each model and 
+  #compare them to their true lables
   lasso_acc<-reactive({
     mean(unlist(answers()) == unlist(lasso_pred_class()))
   })
@@ -311,7 +324,7 @@ shinyServer(function(input, output){
   
   
   #all in a table---- 
-  
+  #Construct a table for the accuracies to be output in the User Interface
   output$table<- renderTable({
     table1<-c(lasso_acc(), svm_acc(), ada_acc(), rf_acc(), obrf_acc())
     table2<-as.data.frame(t(table1))
@@ -319,8 +332,8 @@ shinyServer(function(input, output){
     table2
   }, bordered = TRUE, striped = TRUE, spacing = 'l', width = '100%')
   
-  #Confusion Matrices
-  
+  #Confusion Matrices----------------
+  #Calculate confusion matrices to be used by our four fold plot arguments
   lasso_cm<- reactive({
     confusionMatrix(data=as.factor(unlist(lasso_pred_class())), reference= as.factor(unlist(answers())))
   })
@@ -342,7 +355,8 @@ shinyServer(function(input, output){
   })
   
   #Performance ---------------------------------
-  
+  #Calculate the performance of each model. 
+  #This is to be used in constructing the ROC curve of each model
   lasso_perf<-reactive({
     lasso_pred <- prediction((1-unlist(lasso_pred_prob())), unlist(answers()))
     performance(lasso_pred, 'tpr', 'fpr')
@@ -368,13 +382,12 @@ shinyServer(function(input, output){
     performance(obrf_pred, 'tpr', 'fpr')
   })
   
-  output$type_obrf<-renderPrint({
-    length(obrf_pred_prob()[[1]])
-  })
+  
   
   
   #AUC----------------------------------------
-  
+  #Calculate the AUC values for each of the ROC curves
+  #rounded to 4 places
   lasso_perf_auc<-reactive({
     lasso_pred <- prediction(1-unlist(lasso_pred_prob()), unlist(answers()))
     lasso_perf_auc1<-performance(lasso_pred, 'auc')
@@ -412,8 +425,11 @@ shinyServer(function(input, output){
     head(datasetInput(), n = input$obs)
   })
   
-  #Convert ROC plot to plot_ly
-  #make new vars for data to be plotted
+  
+  #As we are using plotly, a different approach had to be made into constructing the 
+  #ROC plot. 
+  #Here we disect the values of the performance() of each model, 
+  #and plot the TPR against the FPR
   
   lasso_data<-reactive({
     y_lasso<-lasso_perf()@y.values[[1]]
@@ -445,7 +461,8 @@ shinyServer(function(input, output){
     as.data.frame(cbind(x_obrf,y_obrf))
   })
   
-  
+  #Along with plotting the TPR and FPR of each model, 
+  #we include the AUC in the interactive legend of our plot.
   output$roc <- renderPlotly({
     p<-plot_ly(lasso_data(), x = lasso_data()[,1], y = lasso_data()[,2], name = sprintf("Lasso -  %s", lasso_perf_auc()), type = 'scatter', mode = 'lines', line = list(color = 'green', width = 1))%>%
       add_trace(svm_data(), x = svm_data()[,1], y = svm_data()[,2], name = sprintf("SVM -  %s", svm_perf_auc()), mode = 'lines', line = list(color = 'blue', width = 1))%>%
@@ -487,30 +504,32 @@ shinyServer(function(input, output){
                  main = 'Oblique Random Forest')
   })
   
-  #Varible Importance plots
+  #Varible Importance plot for Random Forest
   output$rf_VarImpPlot<- renderPlot({
+    #sort the importance values
     imp_vec1<-sort.int(rf_model()$importance,index.return = TRUE)
+    #extract the names of the importance values in order
     names1<-names(rf_model()$importance[,1])[imp_vec1$ix]
-    par(mar=c(6.1, 15.2 ,4.1 ,2.1))
-    barplot(tail(imp_vec1$x,10), horiz = TRUE, 
-            names.arg =names1[1:10], las = 1, col = 'red',
-            main = 'Random Forest Variable Importance', xlim = c(0, max(imp_vec1$x)))
+    #Allow big margin for lengthly protein names
+    par(mar=c(6.1, 12.2 ,4.1 ,2.1))
+    barplot(tail(imp_vec1$x,20), horiz = TRUE, 
+            names.arg =rev(rev(names(rf_model()$importance[,1])[imp_vec1$ix])[1:20]), las = 1, col = 'red',
+            main = 'Random Forest Variable Importance', xlim = c(0, max(imp_vec1$x) + 1))
   })
   
   
-  #Include a table of variables with their indices
+  # AdaBoost Variable importance plot 
   
   output$ada_VarImpPlot<- renderPlot({
     imp_vec1<-sort.int(ada_model()$importance,index.return = TRUE)
     names1<-names(ada_model()$importance)[imp_vec1$ix]
-    par(mar=c(6.1, 15.2 ,4.1 ,2.1))
-    barplot(tail(imp_vec1$x,10), horiz = TRUE, 
-            names.arg =names1[1:10], las = 1, col = 'red',
-            main = 'AdaBoostM1 Variable Importance', xlim = c(0, max(imp_vec1$x)))
+    par(mar=c(6.1, 12.2 ,4.1 ,2.1))
+    barplot(tail(imp_vec1$x,20), horiz = TRUE, 
+            names.arg = rev(rev(names(ada_model()$importance)[imp_vec1$ix])[1:20]), las = 1, col = 'red',
+            main = 'AdaBoost Variable Importance', xlim = c(0, max(imp_vec2$x) + 2))
   })
   
   
-  #include VarImpPlot of oblique regression
   
   
   #include VarImpPlot of obliqueRF
@@ -519,20 +538,28 @@ shinyServer(function(input, output){
     imp_vec1<-sort.int(obrf_imp,index.return = TRUE)
     drops<- c(input$dv)
     names.arg1 <- colnames(datasetInput()[,!(names(datasetInput()) %in% drops)])[unlist(imp_vec1$ix)]
-    par(mar=c(6.1, 15.2 ,4.1 ,2.1))
+    par(mar=c(6.1, 12.2 ,4.1 ,2.1))
     barplot(rev(head(imp_vec1$x,20)), horiz = TRUE, 
             names.arg = rev(names.arg1[1:20]), las = 1, col = 'red',
             main = 'OBRF Variable Importance',xlim = c(0, min(imp_vec1$x -16)))
   })
   
   
-  
   #include summary of coefficients for lasso/logistic regression
-  output$lasso_summary <- renderPrint({
-    coef(lasso_model(), s = "lambda.min")
+  output$lasso_summary <- renderTable({
+    drops<- c(input$dv)
+    #non-zero variables in our laaao model, given the saelected shrinkage parameter
+    Variables<-c(colnames(datasetInput()[,!(names(datasetInput()) %in% drops)])[which(coef(lasso_model(), s = input$t_value) != 0)])
+    #Coefficients of our variables
+    Coefficients<-c(coef(lasso_cv_model, s = input$t_value)[which(coef(lasso_model(), s = input$t_value) != 0)])
+    tab<-cbind(Variables, Coefficients)
+    tab
     #sig<-coef(lasso_model(), s = "lambda.min")
     #names(sig[sig!=0.0])
   })
+  
+  
+  
 })
 
 
--- ui.R
+++ ui.R
@@ -1,3 +1,6 @@
+#The following is Java Script code for 
+#the logrithim scale of the Cost function for the SVM 
+
 JScode <-
   "$(function() {
 setTimeout(function(){
@@ -14,12 +17,14 @@ $('#svm_cost').data('ionRangeSlider').update({'values':vals})
 
 shinyUI(fluidPage(#theme = 'bootstrap1.css',
   # Application title
+  #CSS script for the font of the title 
   includeCSS("lobster.css"),
   
   headerPanel("Classification of Proteomic Data"),
   # Sidebar
   sidebarLayout(
     sidebarPanel(
+      #file upload section 
       fileInput('file1', 'Choose CSV File',
                 accept=c('text/csv', 
                          'text/comma-separated-values,text/plain', 
@@ -27,17 +32,27 @@ shinyUI(fluidPage(#theme = 'bootstrap1.css',
       uiOutput('dv'),
       
       # added interface for uploading data from
-      tags$br(),
+      tags$br(), 
       
+      #select the number of folds we wish to perform
+      selectInput('k_fold', label = "Number of folds - Cross Validation", 
+                             choices = list('2'=2,'3'=3,'4'=4,'5'=5, 
+                                            '6'=6, '7'=7, '8'=8, '9'=9, '10'=10, 
+                                            n = 'nrows'), selected = '5'),
+      
+      #Check which classification techniques we would like to perform
+      #Currently redundant. Runs for all methods.
       checkboxGroupInput('check', h3('Methods of Classification:'), 
                          choices = list( 'Lasso Regression' = 1, 'Support Vector Machine' = 2, 
                                          'AdaBoost' = 3, 'Random Forest' = 4, 
                                          'Oblique Random Forest' = 5), selected = c(1,2,3,4,5)), 
       HTML('</br>'),
       
+      #Select a value for the lasso shrinkage parameter
       selectInput('t_value', label = "Lasso shrinkage parameter", 
                   choices = list("One S.E." = 'lambda.1se', "Minimum" = 'lambda.min'), selected = 'lambda.1se'),
       
+      #Cost function scale for the SVM
       tags$head(tags$script(HTML(JScode))),
       
       sliderInput('svm_cost',
@@ -46,32 +61,42 @@ shinyUI(fluidPage(#theme = 'bootstrap1.css',
                   max = 10000,
                   value = 5), 
       
+      #Select no. of trees for AdaBoost to run
       sliderInput("ada_tree", "AdaBoost Trees",
-                  min = 0, max = 60, value = 10, step = 2), 
+                  min = 0, max = 100, value = 10, step = 2),
+      #Select no. of trees for the random forest to run
       sliderInput("rf_tree", label = "RF Trees",
                   min = 0, max = 1000, value = 600, step = 50),
+      #Select no. of trees for the Oblique Random Forest
       sliderInput("obrf_tree", "OBliqueRF Trees",
-                  min = 0, max = 60, value = 10, step = 2)),
+                  min = 0, max = 100, value = 10, step = 2)),
     
     #Main panel 
     
     mainPanel(
       tabsetPanel(type = "tabs", 
+                  #Data table tab
                   tabPanel(title = "Data",
                            numericInput("obs", label = h5("Number of observations to view"), 10),
                            tableOutput("contents")),
                   
+                  #Various Univariate statistics
                   tabPanel("Summary Statistics",
                            uiOutput('sumvar'),
                            verbatimTextOutput('summary'), 
+                           h3(' Wilcoxon signed-rank test'),
+                           verbatimTextOutput('wilcoxon'),
                            h2('BoxPlot'),
                            plotlyOutput('boxPlot')),
                   
+                  #Construct accuracy tab
                   tabPanel(title = 'Accuracy',
                            helpText("Below is the classification accuracy of each individual model."
                                     ,br(),br()),
+                           #Output table for accuracies
                            tableOutput('table'),
                            tabsetPanel( id = 'subtab',
+                                        #All corresponding four fold plots 
                                         tabPanel( title = 'Lasso',
                                                   plotOutput('lasso_4fold')),
                                         tabPanel( title = 'SVM',
@@ -84,6 +109,7 @@ shinyUI(fluidPage(#theme = 'bootstrap1.css',
                                                   plotOutput('obrf_4fold')))),
                   
                   tabPanel(title = "ROC Curve", 
+                           #Output ROC Curve
                            plotlyOutput("roc")), 
                   
                   tabPanel(title = 'Variable Importance', 
@@ -91,15 +117,18 @@ shinyUI(fluidPage(#theme = 'bootstrap1.css',
                                     ,br(),br()),
                            tabsetPanel( id = 'subtab', 
                                         tabPanel('Lasso',
-                                                 #helpText("Variables denoted with '.' are deemed insignificant at a 0.05 level of confidence"),
-                                                 #They arent?
-                                                 #Maybe we should just list all non-zero variables???
-                                                 verbatimTextOutput('lasso_summary')),
+                                                 #Output the table of non-zero coefficients 
+                                                 helpText('Variables output have non-zero coefficients.'),
+                                                 helpText('0. may appear where coefficients very small'),
+                                                 tableOutput('lasso_summary')),
                                         tabPanel('Random Forest',
+                                                 #Variable importance plot for Random Forest
                                                  plotOutput('rf_VarImpPlot')), 
                                         tabPanel('AdaBoost', 
+                                                 #Variable importance plot for Ada Boost
                                                  plotOutput('ada_VarImpPlot')), 
                                         tabPanel('ObliqueRF',
+                                                 #Variable importance plot for Oblique Random Forest
                                                  plotOutput('obrf_VarImpPlot')))
                   )
       )
